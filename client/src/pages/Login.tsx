8b39e30d45905c2b638fc598e45e1f4cffdcac95
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-12-01 22:31:06 +0530

    organised imports

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import api from '../api';
 import './Login.css';
 
